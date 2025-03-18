a02679d9bd827f2fcacae263d7aca2c6c2850aa6
JingleGames <jinglegames.reis@gmail.com>
2025-03-18 17:55:32 -0400

    organizando os arquivos nas pastas

--- lista de atividades 2/atividade_07_ponteiro_para_string.cpp
+++ lista de atividades 2/atividade_07_ponteiro_para_string.cpp
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main() {
-	char str[] = "Ola, mundo!";
-	char *ponteiro = str;
+	char str[] = "Ola, mundo!"; // Declaração da String
+	char *ponteiro = str; // Declaração do ponteiro
 
+	// Loop responsável por imprimir os caracteres
 	while(*ponteiro != '\0') {
 		printf("%c ", *ponteiro);
 		ponteiro++;
